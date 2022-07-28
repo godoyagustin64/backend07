d97d942983d210dbc90aa6692ed8c11c8c096ec5
Agustin Godoy <godoyagustinnahuel93@gmail.com>
2022-07-28 19:46:37 -0300

    se agrega nodemon y require

---
+++ src/app.js
@@ -0,0 +1,8 @@
+const expreess = require("express");
+const session = require("express-session");
+const cookie = require ("cookie-parser")
+
+const app = express();
+
+app.listen(3000, () => console.log("Servidor en el puerto 3000"))
+
